b5a257d28a732bfcd0d3adc8f231df2e563ad332
fikir54 <fikir.page@gmail.com>
2022-04-27 19:18:31 +0300

    Harambe Restaurant Website

--- src/Components/About/about.js
+++ src/Components/About/about.js
@@ -3,6 +3,9 @@ import React from 'react'
 const about = () => {
   return (
     <div>this is about</div>
+
+	<div> The word means 'all pull together' in Kiswahili, Kenya's national language. A long-standing Kenyan tradition, a harambee is a type of self-help event that's deeply ingrained in the moral compass of the country. </div>
+
   )
 }
 
